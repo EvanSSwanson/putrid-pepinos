a79b2b681e87381cc05a8f92efa0d6e86038eee8
Evan Swanson <evan.sundelius@gmail.com>
2022-12-01 16:30:20 -0600

    Set up App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,28 @@
-import logo from '../../logo.svg';
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+import movieData from '../../movieData.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      movies: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState({movies: movieData.movies})
+  }
+
+  render() {
+    return (
+      <main className="App">
+        <h1>Hello</h1>
+        {/* <button onClick={() => console.log(this.state.movies)}>🗑</button> */}
+      </main>
+  )}
 }
+
 
 export default App;
