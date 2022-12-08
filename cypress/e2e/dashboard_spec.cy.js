f4c33749553c60e1f5e75eadbe30bf7d5061946d
Josh Pierce <joshua.alan.pierce@gmail.com>
2022-12-07 18:39:54 -0700

    Add fixtures to cypress testing

--- cypress/e2e/dashboard_spec.cy.js
+++ cypress/e2e/dashboard_spec.cy.js
@@ -1,5 +1,21 @@
 describe('My First Test', () => {
-  it('Visits the Putrid Peppinos', () => {
+  beforeEach(() => {
+    cy.intercept("https://rancid-tomatillos.herokuapp.com/api/v2/movies", {
+      method: 'GET',
+      fixture: '../fixtures/movies.json'
+    });
     cy.visit('http://localhost:3000/')
   })
+  it('Should have a title', () => {
+    cy.contains('h1', 'Putrid Pepinos')
+  })
+  it('Show all the available movies', () => {
+    cy.get('.movies-container').within(() => {
+      cy.get('.movie-card').should('have.length', 4)
+      cy.get('.movie-card').eq(0).should('contain', 'Money Plane')
+        .and('contain', '6.7')
+        .and('contain', 'movies.poster_path')
+    })
+  })
 })
+
